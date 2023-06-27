14012e349e45010326d59b0e8650db309dc990de
hans-jacob Melby <hans-jacob.melby@nav.no>
2023-06-27 15:16:47 +0200

    bedre tester, og ferning av grunnlagsdata i test filer

--- src/test/kotlin/no/nav/medlemskap/sykepenger/lytter/service/RegelMotorResponsHandlerTest.kt
+++ src/test/kotlin/no/nav/medlemskap/sykepenger/lytter/service/RegelMotorResponsHandlerTest.kt
@@ -1,8 +1,12 @@
 package no.nav.medlemskap.sykepenger.lytter.service
 
+import io.ktor.server.plugins.statuspages.*
 import no.nav.medlemskap.sykepenger.lytter.config.objectMapper
+import no.nav.medlemskap.sykepenger.lytter.rest.Spørsmål
+import no.nav.medlemskap.sykepenger.lytter.rest.Svar
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import java.util.Collections
 
 class RegelMotorResponsHandlerTest {
     @Test
@@ -19,8 +23,10 @@ class RegelMotorResponsHandlerTest {
     @Test
     fun tredjelandsBorgerGiftMedEOSPerson(){
         val fileContent = this::class.java.classLoader.getResource("UAVKLART_3LAND_GIFTEOS_BORGER.json").readText(Charsets.UTF_8)
-        RegelMotorResponsHandler().interpretLovmeRespons(fileContent)
+        val response = RegelMotorResponsHandler().interpretLovmeRespons(fileContent)
         val jsonNode = objectMapper.readTree(fileContent)
+        Assertions.assertTrue(response.svar == (Svar.UAVKLART))
+        Assertions.assertTrue(response.sporsmal.containsAll(setOf(Spørsmål.OPPHOLDSTILATELSE, Spørsmål.ARBEID_UTENFOR_NORGE,Spørsmål.OPPHOLD_UTENFOR_EØS_OMRÅDE)))
         Assertions.assertFalse(jsonNode.erEosBorger())
         Assertions.assertTrue(jsonNode.erTredjelandsborger())
         Assertions.assertTrue(jsonNode.erTredjelandsborgerMedEØSFamilie())
@@ -29,8 +35,10 @@ class RegelMotorResponsHandlerTest {
     @Test
     fun tredjelandsBorgerIkkeGiftMedEOSPerson(){
         val fileContent = this::class.java.classLoader.getResource("UAVKLART_3LAND_IKKE_GIFT_EOS_BORGER.json").readText(Charsets.UTF_8)
-        RegelMotorResponsHandler().interpretLovmeRespons(fileContent)
+        val response = RegelMotorResponsHandler().interpretLovmeRespons(fileContent)
         val jsonNode = objectMapper.readTree(fileContent)
+        Assertions.assertTrue(response.svar == (Svar.UAVKLART))
+        Assertions.assertTrue(response.sporsmal.containsAll(setOf(Spørsmål.OPPHOLDSTILATELSE, Spørsmål.ARBEID_UTENFOR_NORGE,Spørsmål.OPPHOLD_UTENFOR_NORGE)))
         Assertions.assertFalse(jsonNode.erEosBorger())
         Assertions.assertTrue(jsonNode.erTredjelandsborger())
         Assertions.assertFalse(jsonNode.erTredjelandsborgerMedEØSFamilie())
@@ -39,8 +47,10 @@ class RegelMotorResponsHandlerTest {
     @Test
     fun regel_19_1(){
         val fileContent = this::class.java.classLoader.getResource("REGEL_19_1.json").readText(Charsets.UTF_8)
-        RegelMotorResponsHandler().interpretLovmeRespons(fileContent)
+        val response  = RegelMotorResponsHandler().interpretLovmeRespons(fileContent)
         val jsonNode = objectMapper.readTree(fileContent)
+        Assertions.assertTrue(response.svar == (Svar.UAVKLART))
+        Assertions.assertTrue(response.sporsmal.containsAll(setOf(Spørsmål.OPPHOLDSTILATELSE, Spørsmål.ARBEID_UTENFOR_NORGE,Spørsmål.OPPHOLD_UTENFOR_NORGE)))
         Assertions.assertFalse(jsonNode.erEosBorger())
         Assertions.assertTrue(jsonNode.erTredjelandsborger())
         Assertions.assertFalse(jsonNode.erTredjelandsborgerMedEØSFamilie())
@@ -49,8 +59,10 @@ class RegelMotorResponsHandlerTest {
     @Test
     fun regel_19_8(){
         val fileContent = this::class.java.classLoader.getResource("REGEL_19_8.json").readText(Charsets.UTF_8)
-        RegelMotorResponsHandler().interpretLovmeRespons(fileContent)
+        val response = RegelMotorResponsHandler().interpretLovmeRespons(fileContent)
         val jsonNode = objectMapper.readTree(fileContent)
+        Assertions.assertTrue(response.svar == (Svar.UAVKLART))
+        Assertions.assertTrue(response.sporsmal.containsAll(setOf(Spørsmål.OPPHOLDSTILATELSE, Spørsmål.ARBEID_UTENFOR_NORGE,Spørsmål.OPPHOLD_UTENFOR_NORGE)))
         Assertions.assertFalse(jsonNode.erEosBorger())
         Assertions.assertTrue(jsonNode.erTredjelandsborger())
         Assertions.assertFalse(jsonNode.erTredjelandsborgerMedEØSFamilie())
@@ -59,7 +71,9 @@ class RegelMotorResponsHandlerTest {
     @Test
     fun regel_19_7(){
         val fileContent = this::class.java.classLoader.getResource("REGEL_19_7.json").readText(Charsets.UTF_8)
-        RegelMotorResponsHandler().interpretLovmeRespons(fileContent)
+        val response = RegelMotorResponsHandler().interpretLovmeRespons(fileContent)
+        Assertions.assertTrue(response.svar == (Svar.UAVKLART))
+        Assertions.assertTrue(response.sporsmal.containsAll(setOf(Spørsmål.OPPHOLDSTILATELSE, Spørsmål.ARBEID_UTENFOR_NORGE,Spørsmål.OPPHOLD_UTENFOR_EØS_OMRÅDE)))
         val jsonNode = objectMapper.readTree(fileContent)
         Assertions.assertTrue(jsonNode.erBritiskBorger())
         Assertions.assertFalse(jsonNode.harOppholdsTilatelse())
